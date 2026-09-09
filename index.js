26a1071b58e35317b051475b33a258e42e444f4f
Gautam kumar <gautamsiddharth2013@gmail.com>
2026-09-09 08:59:37 +0530

    added url

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const loanPaymentRoutes = require('./routes/loanPaymentRoutes')
 
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5174', 'http://localhost:5173'],
+    origin: [`${process.env.FRONTEND_URL}`, 'http://localhost:5173'], 
     credentials: true,
 }))
 app.use(cookieParser())
